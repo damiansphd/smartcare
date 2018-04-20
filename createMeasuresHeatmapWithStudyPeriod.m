cdad5ef8134ac1604e43de11f64cbd113d886af7
damiansphd <37808691+damiansphd@users.noreply.github.com>
2018-04-20 08:24:11 +0100

    Add files via upload
    one final fix + added comments

--- createMeasuresHeatmapWithStudyPeriod.m
+++ createMeasuresHeatmapWithStudyPeriod.m
@@ -21,33 +21,43 @@ colors(8,:)  = temp(18,:) .* brightness;
 colors(9,:)  = temp(20,:) .* brightness;
 colors(10,:)  = [1 0 1];
 
+% get the date scaling offset for each patient
 patientoffsets = getPatientOffsets(physdata);
 
+% create a table of counts of measures by patient/day (@max function here
+% is irrelevant as we just want the group counts
 pdcountmtable = varfun(@max, physdata(:, {'SmartCareID','ScaledDateNum'}), 'GroupingVariables', {'SmartCareID', 'ScaledDateNum'});
 
+% extract study date and join with offsets to keep only those patients who
+% have enough data (ie the patients left after outlier date handling
 patientstudydate = sortrows(cdPatient(:,{'ID', 'StudyDate'}), 'ID', 'ascend');
 patientstudydate.Properties.VariableNames{'ID'} = 'SmartCareID';
-
 patientstudydate = innerjoin(patientoffsets, patientstudydate);
 
+% create a scaleddatenum to translate the study date to the same normalised
+% scale as measurement data scaled date num
 patientstudydate.ScaledDateNum = datenum(patientstudydate.StudyDate) - offset - patientstudydate.PatientOffset;
 
+% add rows to the count table to mark the study start and end dates (use a count
+% of 10 to allow it to be highlighted in a different colour on the heatmap
 fixedcount = ones(size(patientstudydate,1),1)*10;
 fixedcount = array2table(fixedcount);
 fixedcount.Properties.VariableNames{'fixedcount'} = 'GroupCount';
-
-
 rowstoadd = [patientstudydate(:,{'SmartCareID', 'ScaledDateNum'}) fixedcount];   
 pdcountmtable = [pdcountmtable ; rowstoadd];
 rowstoadd.ScaledDateNum = rowstoadd.ScaledDateNum + 183;
 pdcountmtable = [pdcountmtable ; rowstoadd];
 
+% create the min and max smartcareid to allow me to hide the dummy row
+% below
 dispmin = min(pdcountmtable.SmartCareID);
 dispmax = max(pdcountmtable.SmartCareID);
 
+% add dummy rows to create a record for every day in the range of the data
+% so the heatmap is scaled correctly for missing days
+% but excluded from display limits so the row doesn't show on the heatmap
 dummymin = min(pdcountmtable.ScaledDateNum);
 dummymax = max(pdcountmtable.ScaledDateNum);
-%dummymeasures = table('Size',[dummymax-dummymin+1 3], 'VariableTypes', {'int32', 'int32', 'int32'}, 'VariableNames', {'SmartCareID', 'ScaledDateNum', 'GroupCount'});
 dummymeasures = pdcountmtable(1:dummymax-dummymin+1,:);
 dummymeasures.SmartCareID(:) = 0;
 dummymeasures.GroupCount(:) = 1;
@@ -56,6 +66,7 @@ for i = 1:dummymax-dummymin+1
 end
 pdcountmtable = [pdcountmtable ; dummymeasures];
 
+% create the heatmap
 title = 'Heatmap of Measures with Study Period';
 f = figure('Name', title);
 p = uipanel('Parent',f,'BorderType','none'); 
@@ -76,5 +87,6 @@ h.GridVisible = 'off';
 
 %[C,x] = sortx(h);
 
+% save results
 filename = 'HeatmapAllPatientsWithStudyPeriod.png';
 saveas(f,filename);
