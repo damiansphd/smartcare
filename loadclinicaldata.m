9d89fb1b06d72f41264f90b9047e2feb2737d06e
damiansphd <37808691+damiansphd@users.noreply.github.com>
2018-05-02 17:10:26 +0100

    Add files via upload
    Intermediate commit for work on Patient Summary visualisation

--- loadclinicaldata.m
+++ loadclinicaldata.m
@@ -16,20 +16,28 @@ tic
 % load relevant clinical data
 fprintf('Loading relevant clinical data\n');
 cdPatient = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdpatientsheet);
+cdPatient = sortrows(cdPatient, {'ID'}, 'ascend');
 fprintf('Patient data has %d rows\n',size(cdPatient,1));
 cdMicrobiology = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdmicrosheet);
+cdMicrobiology = sortrows(cdMicrobiology, {'ID', 'DateMicrobiology'}, 'ascend');
 fprintf('Microbiology data has %d rows\n',size(cdMicrobiology,1));
 cdClinicVisits = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdcvsheet);
+cdClinicVisits = sortrows(cdClinicVisits, {'ID', 'AttendanceDate'}, 'ascend');
 fprintf('Clinic Visits data has %d rows\n',size(cdClinicVisits,1));
 cdPFT = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdpftsheet);
+cdPFT = sortrows(cdPFT, {'ID', 'LungFunctionDate'}, 'ascend');
 fprintf('PFT data has %d rows\n',size(cdPFT,1));
 cdAdmissions = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdadmisssheet);
+cdAdmissions = sortrows(cdAdmissions, {'ID', 'Admitted'}, 'ascend');
 fprintf('Admissions data has %d rows\n',size(cdAdmissions,1));
 cdAntibiotics = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdantibsheet);
+cdAntibiotics = sortrows(cdAntibiotics, {'ID', 'StartDate'}, 'ascend');
 fprintf('Antibiotics data has %d rows\n',size(cdAntibiotics,1));
 cdCRP = readtable(fullfile(basedir, subfolder, clinicaldatafile), 'Sheet', cdcrpsheet);
+cdCRP = sortrows(cdCRP, {'ID', 'CRPDate'}, 'ascend');
 fprintf('CRP data has %d rows\n',size(cdCRP,1));
 cdEndStudy = readtable(fullfile(basedir, subfolder, endofstudyfile));
+cdEndStudy = sortrows(cdEndStudy, {'ID'}, 'ascend');
 fprintf('End of Study data has %d rows\n',size(cdEndStudy,1));
 toc
 fprintf('\n');
