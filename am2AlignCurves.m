e708d522e4d8e0247b85b5ec5bf4f57df52539b8
damiansphd <37808691+damiansphd@users.noreply.github.com>
2018-06-12 13:54:20 +0100

    Add files via upload
    Initial work on an updated model based on feedback from John

---
+++ am2AlignCurves.m
@@ -0,0 +1,103 @@
+function [offsets, profile_pre, profile_post, hstg, qual] = am2AlignCurves(amNormcube, amInterventions, measures, max_offset, align_wind, nmeasures, ninterventions, run_type, detaillog)
+
+% alignCurves = function to align measurement curves prior to intervention
+
+meancurvesum      = zeros(max_offset + align_wind, nmeasures);
+meancurvecount    = zeros(max_offset + align_wind, nmeasures);
+offsets           = zeros(ninterventions, 1);
+profile_pre       = zeros(nmeasures, max_offset+align_wind);
+profile_post      = zeros(nmeasures, max_offset+align_wind);
+hstg              = zeros(nmeasures, ninterventions, max_offset);
+qual = 0;
+
+% calculate mean curve over all interventions
+for i = 1:ninterventions
+    [meancurvesum, meancurvecount] = am2AddToMean(meancurvesum, meancurvecount, amNormcube, amInterventions, i, max_offset, align_wind, nmeasures);
+end
+
+% store the mean curves pre-alignment for each measure for plotting
+for m = 1:nmeasures
+    for day = 1:max_offset + align_wind
+        profile_pre(m, day) = meancurvesum(day, m)/meancurvecount(day, m);
+    end
+end
+
+% iterate to convergence
+pnt = 1;
+cnt = 0;
+iter = 0;
+ok  = 0;
+while 1
+    [meancurvesum, meancurvecount] = am2RemoveFromMean(meancurvesum, meancurvecount, amNormcube, amInterventions, pnt, max_offset, align_wind, nmeasures);
+    %check safety
+    ok = 1;
+    for i=1:max_offset + align_wind
+        for m=1:nmeasures
+            if meancurvecount(i,m) < 3
+                %if detaillog
+                %    fprintf('Intervention %d, Measure %s, dayprior %d <3 datapoints\n', pnt, measures.Name{m}, i);
+                %end
+                ok = 0;
+            end
+        end
+    end
+    
+    if ok == 1
+        [better_offset, hstg] = am2BestFit(meancurvesum, meancurvecount, amNormcube, amInterventions, hstg, pnt, max_offset, align_wind, nmeasures);
+    else
+        better_offset = amInterventions.Offset(pnt);
+    end
+    
+    if better_offset ~= amInterventions.Offset(pnt)
+        amInterventions.Offset(pnt) = better_offset;
+        %if detaillog
+        %    fprintf('amIntervention.Offset(%d) set to %d\n', pnt, better_offset);
+        %end
+        cnt = cnt+1;
+    end
+    [meancurvesum, meancurvecount] = am2AddToMean(meancurvesum, meancurvecount, amNormcube, amInterventions, pnt, max_offset, align_wind, nmeasures);
+        
+    pnt = pnt+1;
+    if pnt > ninterventions
+        iter = iter + 1;
+        pnt = pnt - ninterventions;
+        if cnt == 0
+            if detaillog
+                fprintf('Converged after %2d iterations\n', iter);
+            end
+            break;
+        else 
+            if detaillog
+                fprintf('Changed %2d offsets on iteration %2d\n', cnt, iter);
+            end
+            if iter > 100
+                if detaillog
+                    fprintf('Iteration count limit exceeded - breaking\n');
+                end
+                break;
+            end
+            cnt = 0;
+        end
+    end
+end
+
+%computing the objective function result for converged offset array
+for i=1:ninterventions
+    [meancurvesum, meancurvecount] = am2RemoveFromMean(meancurvesum, meancurvecount, amNormcube, amInterventions, i, max_offset, align_wind, nmeasures);
+    qual = qual + am2CalcObjFcn(meancurvesum, meancurvecount, amNormcube, amInterventions, hstg, i, amInterventions.Offset(i), max_offset, align_wind, nmeasures, 0);
+    [meancurvesum, meancurvecount] = am2AddToMean(meancurvesum, meancurvecount, amNormcube, amInterventions, i, max_offset, align_wind, nmeasures);
+end
+
+for i=1:ninterventions 
+    offsets(i) = amInterventions.Offset(i);
+end
+
+% store the mean curves post-alignment for each measure for plotting
+for m = 1:nmeasures
+    for day = 1:max_offset + align_wind
+        profile_post(m, day) = meancurvesum(day, m)/meancurvecount(day, m);
+    end
+end
+
+end
+
---
+++ am2CalcObjFcn.m
@@ -0,0 +1,32 @@
+function [dist, hstg] = am2CalcObjFcn(meancurvesum, meancurvecount, amNormcube, amInterventions, hstg, currinter, curroffset, max_offset, align_wind, nmeasures, updatehistogram)
+
+% amCalcObjFcn - calculates residual sum of squares distance for points in
+% curve vs meancurve incorporating offset
+
+dist = 0;
+scid   = amInterventions.SmartCareID(currinter);
+start = amInterventions.IVScaledDateNum(currinter);
+
+if (updatehistogram == 1)
+    for m = 1:nmeasures
+        hstg(m, currinter, curroffset + 1) = 0;
+    end
+end
+
+for i = 1:align_wind
+    for m = 1:nmeasures
+        if start - i <= 0
+            continue;
+        end
+        if ~isnan(amNormcube(scid, start - i, m))
+            thisdist = (meancurvesum((max_offset + align_wind + 1) - i - curroffset, m)/meancurvecount((max_offset + align_wind + 1) - i - curroffset, m) ...
+                - amNormcube(scid, start - i, m))^2;
+            dist = dist + thisdist;
+            if (updatehistogram == 1)
+                hstg(m, currinter, curroffset + 1) = hstg(m, currinter, curroffset + 1) + thisdist;
+            end
+        end
+    end
+end
+
+end
