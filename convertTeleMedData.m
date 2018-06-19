19e0bb7a2a9142f824c663842d6bfba928bc6dc2
damiansphd <37808691+damiansphd@users.noreply.github.com>
2018-06-19 08:52:10 +0100

    Add files via upload
    Work to ingest Telemed data

---
+++ convertTeleMedData.m
@@ -0,0 +1,149 @@
+function [tmClinicVisits, tmAdmissions, tmAntibiotics, tmCRP, tmPFT] = convertTeleMedData(tmData, tmPatient, tmClinicVisits, tmAdmissions, tmAntibiotics, tmCRP, tmPFT, ...
+    cvrowtoadd, admrowtoadd, poabrowtoadd, ivabrowtoadd, crprowtoadd, pftrowtoadd, fileid)
+
+% convertTelemedData - converts from TelemedData format to SmartCare format
+
+if isequal(class(tmData.CRP), 'cell')
+    numlevel = str2double(regexprep(tmData.CRP, '[<>]',''));
+else
+    numlevel = tmData.CRP;
+end
+numlevel = array2table(numlevel);
+numlevel.Properties.VariableNames{1} = 'NumericLevel';
+tmData = [tmData numlevel];
+
+nmeasurements = size(tmData,1);
+
+id = tmPatient.ID(tmPatient.ID==fileid);
+
+prioradm = {''};
+priorpoab = {''};
+priorivab = {''};
+
+for i = 1:nmeasurements
+    
+    % Clinic Visits
+    if isequal(class(tmData.Clinic),'cell')
+        if (tmData.Clinic{i} == 'Y')
+           cvrowtoadd.ID = id;
+           cvrowtoadd.Hospital = 'PAP';
+           cvrowtoadd.StudyNumber = ' ';
+           cvrowtoadd.ClinicID = 0;
+           cvrowtoadd.AttendanceDate = tmData.Date(i);
+           tmClinicVisits = [tmClinicVisits; cvrowtoadd];   
+           fprintf('Clinic Visit on %s\n', datestr(tmData.Date(i),29));
+        end
+    end
+    % Hospital Admissions
+    if isequal(class(tmData.Admission),'cell')
+        if (ismember(tmData.Admission(i),{'Y'}) & ~ismember(tmData.Admission(i),prioradm))
+            admrowtoadd.ID = id;
+            admrowtoadd.Hospital = 'PAP';
+            admrowtoadd.StudyNumber = ' ';
+            admrowtoadd.HospitalAdmissionID = 0;
+            admrowtoadd.Admitted = tmData.Date(i);
+            fprintf('Admission started on %s\n', datestr(tmData.Date(i),29));     
+        end
+        if (~ismember(tmData.Admission(i),{'Y'}) & ~ismember(tmData.Admission(i),prioradm))
+            admrowtoadd.Discharge = tmData.Date(i-1);
+            tmAdmissions = [tmAdmissions; admrowtoadd];
+            fprintf('Admission ended on %s\n', datestr(tmData.Date(i-1),29));
+        end 
+        prioradm = tmData.Admission(i);
+    end
+    
+    % Oral Antibiotics
+    if isequal(class(tmData.POAbx),'cell')
+        if (ismember(tmData.POAbx(i),{'Y'}) & ~ismember(tmData.POAbx(i),priorpoab))
+            poabrowtoadd.ID = id;
+            poabrowtoadd.Hospital = 'PAP';
+            poabrowtoadd.StudyNumber = ' ';
+            poabrowtoadd.AntibioticID = 0;
+            poabrowtoadd.AntibioticName = 'Not Captured';
+            poabrowtoadd.Route = 'Oral';
+            poabrowtoadd.HomeIV_s_ = 'No';
+            poabrowtoadd.StartDate = tmData.Date(i);
+            fprintf('Oral AB started on %s\n', datestr(tmData.Date(i),29));     
+        end
+        if (~ismember(tmData.POAbx(i),{'Y'}) & ~ismember(tmData.POAbx(i),priorpoab))
+            poabrowtoadd.StopDate = tmData.Date(i-1);
+            tmAntibiotics = [tmAntibiotics; poabrowtoadd];
+            fprintf('Oral AB ended on %s\n', datestr(tmData.Date(i-1),29));
+        end 
+        priorpoab = tmData.POAbx(i);
+    end
+    
+    % IV Antibiotics
+    if isequal(class(tmData.IVAbx),'cell')
+        if (ismember(tmData.IVAbx(i),{'Y'}) & ~ismember(tmData.IVAbx(i),priorivab))
+            ivabrowtoadd.ID = id;
+            ivabrowtoadd.Hospital = 'PAP';
+            ivabrowtoadd.StudyNumber = ' ';
+            ivabrowtoadd.AntibioticID = 0;
+            ivabrowtoadd.AntibioticName = 'Not Captured';
+            ivabrowtoadd.Route = 'IV';
+            if isequal(class(tmData.Admission),'cell') & ismember(tmData.Admission(i),{'Y'})
+                ivabrowtoadd.HomeIV_s_ = 'No';
+            else
+                ivabrowtoadd.HomeIV_s_ = 'Yes';
+            end
+            ivabrowtoadd.StartDate = tmData.Date(i);
+            fprintf('IV AB started on %s\n', datestr(tmData.Date(i),29));     
+        end
+        if (~ismember(tmData.IVAbx(i),{'Y'}) & ~ismember(tmData.IVAbx(i),priorivab))
+            ivabrowtoadd.StopDate = tmData.Date(i-1);
+            if isequal(class(tmData.Admission),'cell') & ismember(tmData.Admission(i-1),{'Y'}) & ismember(ivabrowtoadd.HomeIV_s_, {'Yes'})
+                   ivabrowtoadd.HomeIV_s_ = 'IP+OP';
+            end
+            if isequal(class(tmData.Admission),'cell') & ~ismember(tmData.Admission(i-1),{'Y'}) & ismember(ivabrowtoadd.HomeIV_s_, {'No'})
+                ivabrowtoadd.HomeIV_s_ = 'IP+OP';
+            end
+            tmAntibiotics = [tmAntibiotics; ivabrowtoadd];
+            fprintf('IV AB ended on %s\n', datestr(tmData.Date(i-1),29));
+        end 
+        priorivab = tmData.IVAbx(i);
+    end
+    
+    % CRP
+    if ~isnan(tmData.NumericLevel(i))
+        crprowtoadd.ID = id;
+        crprowtoadd.Hospital = 'PAP';
+        crprowtoadd.StudyNumber = ' ';
+        crprowtoadd.CRPID = 0;
+        crprowtoadd.CRPDate = tmData.Date(i);
+        if isequal(class(tmData.CRP), 'cell')
+            crprowtoadd.Level = tmData.CRP(i);
+        else
+            crprowtoadd.Level = num2str(tmData.CRP(i));
+        end
+        crprowtoadd.Units = 'mg/L';
+        if (isequal(class(tmData.POAbx),'cell') & ismember(tmData.POAbx(i),{'Y'})) | (isequal(class(tmData.IVAbx),'cell') & ismember(tmData.IVAbx(i), {'Y'}))
+            crprowtoadd.PatientAntibiotics = 'On';
+        else
+            crprowtoadd.PatientAntibiotics = 'Off';
+        end
+        crprowtoadd.NumericLevel = tmData.NumericLevel(i);
+        tmCRP = [tmCRP; crprowtoadd];   
+        fprintf('CRP taken on %s\n', datestr(tmData.Date(i),29));
+    end
+    
+    % PFT
+    if ~isnan(tmData.FEVI_L_(i))
+        pftrowtoadd.ID = id;
+        pftrowtoadd.Hospital = 'PAP';
+        pftrowtoadd.StudyNumber = ' ';
+        pftrowtoadd.LungFunctionID = 0;
+        pftrowtoadd.LungFunctionDate = tmData.Date(i);
+        pftrowtoadd.FEV1 = tmData.FEVI_L_(i);
+        pftrowtoadd.FEV1_ = tmData.FEV1___(i);
+        pftrowtoadd.FVC1 = NaN;
+        pftrowtoadd.FVC1 = NaN;
+        pftrowtoadd.CalcFEV1SetAs = tmPatient.CalcFEV1SetAs(tmPatient.ID==id);
+        pftrowtoadd.CalcFEV1_ = round((pftrowtoadd.FEV1 / pftrowtoadd.CalcFEV1SetAs) * 100);
+        tmPFT = [tmPFT; pftrowtoadd];   
+        fprintf('PFT taken on %s\n', datestr(tmData.Date(i),29));
+    end
+end
+
+end
+
--- loadTeleMeddata.m
+++ loadTeleMeddata.m
@@ -10,22 +10,22 @@ fprintf('Loading equivalent smartcare data structures as baseline\n');
 load(fullfile(basedir, subfolder, clinicalmatfile));
 fprintf('Done\n');
 tmAdmissions        = cdAdmissions;
+admrowtoadd         = tmAdmissions(1,:);
 tmAdmissions(:,:)   = [];
 tmAntibiotics       = cdAntibiotics;
+ivabrowtoadd        = tmAntibiotics(1,:);
+poabrowtoadd        = tmAntibiotics(1,:);
 tmAntibiotics(:,:)  = [];
 tmClinicVisits      = cdClinicVisits;
+cvrowtoadd         = tmClinicVisits(1,:); 
 tmClinicVisits(:,:) = [];
 tmCRP               = cdCRP;
+crprowtoadd         = tmCRP(1,:);
 tmCRP(:,:)          = [];
-tmEndStudy          = cdEndStudy;
-tmEndStudy(:,:)     = [];
-tmMicrobiology      = cdMicrobiology;
-tmMicrobiology(:,:) = [];
-tmOtherVisits       = cdOtherVisits;
-tmOtherVisits(:,:)  = [];
 tmPatient           = cdPatient;
 tmPatient(:,:)      = [];
 tmPFT               = cdPFT;
+pftrowtoadd         = tmPFT(1,:);
 tmPFT(:,:)          = [];
 toc
 fprintf('\n');
@@ -33,24 +33,52 @@ fprintf('\n');
 tic
 basedir = './';
 subfolder = 'DataFiles/TeleMed';
-clinicalfile = 'FEVdata.xlsx';
-tmdatafile1 = 'K1.xlsx';
+clinicalfile = 'FEVdata-complete.xlsx';
 
-fprintf('Loading Telemed clinical patient data');
+fprintf('Loading Telemed clinical patient data\n');
 tmPatientInfo = readtable(fullfile(basedir, subfolder, clinicalfile));
 tmPatientInfo(:,{'AZM','inhPx','PNL','Diabetes'}) = [];
 tmPatientInfo.ID = str2double(strrep(tmPatientInfo.Patient, 'K', ''));
+tmPatientInfo = sortrows(tmPatientInfo,{'ID'});
 tmPatientInfo.PredFEV1 = tmPatientInfo.FEV1_L_Av ./ (tmPatientInfo.FEV1___Av/100);
 tmPatientInfo.FEV1SetAs = round(tmPatientInfo.PredFEV1,1);
 
 ntmpatients = size(tmPatientInfo,1);
-
 tmPatient.ID(1:ntmpatients) = tmPatientInfo.ID;
+for i = 1:size(tmPatient,1)
+    tmPatient.Hospital{i} = 'PAP';
+end
 tmPatient.Age = tmPatientInfo.Age;
 tmPatient.Sex = tmPatientInfo.Gender;
+tmPatient.Height = tmPatientInfo.Height;
+tmPatient.PredictedFEV1 = tmPatientInfo.PredFEV1;
+tmPatient.FEV1SetAs = tmPatientInfo.FEV1SetAs;
 
+fprintf('Populate calculated PredictedFEV1 and FEV1SetAs\n');
+tmMalePatient = tmPatient(ismember(tmPatient.Sex,'M'),:);
+tmFemalePatient = tmPatient(~ismember(tmPatient.Sex,'M'),:);
+tmMalePatient.CalcPredictedFEV1 = (tmMalePatient.Height * 0.01 * 4.3) - (tmMalePatient.Age * 0.029) - 2.49;
+tmFemalePatient.CalcPredictedFEV1 = (tmFemalePatient.Height * 0.01 * 3.95) - (tmFemalePatient.Age * 0.025) - 2.6;
+tmMalePatient.CalcFEV1SetAs = round(tmMalePatient.CalcPredictedFEV1,1);
+tmFemalePatient.CalcFEV1SetAs = round(tmFemalePatient.CalcPredictedFEV1,1);
+tmPatient = sortrows([tmMalePatient ; tmFemalePatient], {'ID'}, 'ascend');
 
 fprintf('\n');
+tic
+for i = 1:ntmpatients
+    tmdatafile = sprintf('K%d.xlsx', i);
+    fprintf('Loading data for Patient K%d\n', i);
+    tmData = readtable(fullfile(basedir, subfolder, tmdatafile));
+    tmData(:,{'PEANg_ml', 'IVAminophylline', 'POTheophylline', 'Steroids', 'WCC', 'Fat', 'Bone', 'Muscle', 'Visceral', 'peakFlow', 'Calories', 'Distance', 'Duration'}) = []; 
+    if i ~= 11 & i ~= 15
+        tmData.Date = datetime(tmData.Date, 'InputFormat', 'dd.MM.yy');
+    end
+    [tmClinicVisits, tmAdmissions, tmAntibiotics, tmCRP, tmPFT] = convertTeleMedData(tmData, tmPatient, tmClinicVisits, tmAdmissions, tmAntibiotics, tmCRP, tmPFT, ...
+            cvrowtoadd, admrowtoadd, poabrowtoadd, ivabrowtoadd, crprowtoadd, pftrowtoadd, i);
+    fprintf('\n');
+        
+end
+
 toc
 
 
