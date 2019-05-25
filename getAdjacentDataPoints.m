1d667c87ff5d6d61b67604a26d541c6f5ad02ea3
damiansphd <37808691+damiansphd@users.noreply.github.com>
2019-05-25 12:29:30 +0100

    Version control issue due to one drive crashing...

--- getAdjacentDataPoints.m
+++ getAdjacentDataPoints.m
@@ -1,4 +1,4 @@
-function [adjsumsqpt, adjsumpt, adjcountpt] = getAdjacentDataPoints(meancurvesumsqrow, meancurvesumrow, meancurvecountrow, thispoint, thismeasure, countthreshold, max_offset, align_wind)
+function [adjsumsqpt, adjsumpt, adjcountpt, range] = getAdjacentDataPoints(meancurvesumsqrow, meancurvesumrow, meancurvecountrow, thispoint, thismeasure, countthreshold, max_offset, align_wind)
 
 % getAdjacentDataPoints - get and store adjacent data points to a point in the latent
 % curve - to be used when there are too few underlying curves contributing
@@ -14,11 +14,11 @@ while (meancurvecountrow(thispoint) + adjcountpt) < countthreshold
         %fprintf('***** Measure %d Day %d, CurveCount %f AdjCount %f, Range %d: Not enough data points to borrow *****\n', thismeasure, thispoint, meancurvecountrow(thispoint), adjcountpt, range);
         break
     end
-    if (thispoint + range) <= max_offset + align_wind - 1
-        adjsumsqpt = adjsumsqpt + meancurvesumsqrow(thispoint + range);
-        adjsumpt   = adjsumpt   + meancurvesumrow(thispoint   + range);
-        adjcountpt = adjcountpt + meancurvecountrow(thispoint + range);
-    end
+    %if (thispoint + range) <= max_offset + align_wind - 1
+    %    adjsumsqpt = adjsumsqpt + meancurvesumsqrow(thispoint + range);
+    %    adjsumpt   = adjsumpt   + meancurvesumrow(thispoint   + range);
+    %    adjcountpt = adjcountpt + meancurvecountrow(thispoint + range);
+    %end
     if (thispoint - range) >= 1
         adjsumsqpt = adjsumsqpt + meancurvesumsqrow(thispoint - range);
         adjsumpt   = adjsumpt   + meancurvesumrow(thispoint   - range);
