96e8ec6c88d418cbaaabc344e82e3684230f095a
damiansphd <37808691+damiansphd@users.noreply.github.com>
2020-01-31 09:26:33 +0000

    temp updates for laptop issue

--- getLatestBreatheDates.m
+++ getLatestBreatheDates.m
@@ -3,9 +3,9 @@ function [clinicaldate, measdate, guidmapdate] = getLatestBreatheDates()
 % getLatestBreatheDates - convenience function to centralise getting the
 % latest breathe clinical and measurement filename date suffixes
 
-clinicaldate = '20191216';
-measdate     = '20200106';
-guidmapdate  = '20200106';
+clinicaldate = '20200109';
+measdate     = '20200120';
+guidmapdate  = '20200127';
 
 
 end
